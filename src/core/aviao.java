75318620ea77937217991738b5224d7c23ab6162
WSantos79 <58752564+WSantos79@users.noreply.github.com>
2020-11-07 12:27:11 -0800

    Updade
    adcionei inimigos,  e fiz algumas melhorias

--- src/core/aviao.java
+++ src/core/aviao.java
@@ -8,20 +8,6 @@ package core;
 public class aviao extends objetos {
 
 	private float velocidade = 50;
-	private final int altura = 72;
-	private final int largura = 60;
-
-	public boolean avaliaPosicao(int posX, int posY) {
-		posX = posX + largura / 2;
-		posY = posY + altura / 2;
-
-		// lado << || lado >> || lado ^^^ || lado vvv
-		if (posX < 30 || posX > 1279.90 || posY < 30 || posY > 835.00) {
-			return false; // Delimitação da janela do jogo
-		}
-
-		return true;
-	}
 
 	public float getVelocidade() {
 		return velocidade;
--- src/main/program.java
+++ src/main/program.java
@@ -11,18 +11,30 @@ import core.aviao;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+
+import java.util.TimerTask;
+
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class program extends Application {
+
+	private static final Random RAND = new Random();
+
+	private int pontos = 0;
 
 	private final String IMG_FUNDO = "recursos/1280.png";
 	private final String IMG_AVIAO = "recursos/aviao.png";
@@ -36,9 +48,22 @@ public class program extends Application {
 	private final world mundo = new world(1262, 825);
 	private final aviao aviao0 = new aviao(600, 750);
 
+
+
 	List<Circle> pShoots = new ArrayList<Circle>();
+	Circle dotR = new Circle();
+	List<ImageView> enemies0 = new ArrayList<ImageView>();
+	List<ImageView> enemies1 = new ArrayList<ImageView>();
 	Pane root = new Pane();
 	AnimationTimer timer;
+
+	Timer timer2 = new Timer();
+
+	boolean isMove = true;
+
+	// para controlar o add de inimigos
+	int b = 0;
+	int a = 0;
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -50,7 +75,13 @@ public class program extends Application {
 			viewAviao.setTranslateY(aviao0.getPosicaoY());
 
 			root.getChildren().add(viewFundo);
-			root.getChildren().add(viewAviao);
+			root.getChildren().add(viewAviao); // cria aviao principal
+			
+
+			
+			 //cria inimigos 
+			addInimigo0();
+			addInimigo1();
 
 			Scene scene = new Scene(root, mundo.DIM_X, mundo.DIM_Y);
 
@@ -58,11 +89,12 @@ public class program extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.show();
 
-			// animaçao
+			// animacao
 			timer = new AnimationTimer() {
 				@Override
 				public void handle(long arg0) {
 					gameUpdate();
+
 				}
 			};
 			timer.start();
@@ -115,27 +147,62 @@ public class program extends Application {
 				if (evt.getCode() == KeyCode.SPACE) {
 					aviaoShoot(aviao0.getPosicaoX(), aviao0.getPosicaoY());
 				}
+				
+
 			});
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	// classe que adciona os inimigos 
+	public void addenemy0() {
+
+		enemies0.add(enemies0(RAND.nextInt(mundo.DIM_X - 40), -40)); // x = cima... y = lados
+		root.getChildren().add((Node) enemies0.get(a));
+		a += 1;
+		enemies0.add(enemies0(RAND.nextInt(mundo.DIM_X - 40), -40)); // x = cima... y = lados
+		root.getChildren().add((Node) enemies0.get(a));
+		a += 1;
+
+	}
+	// classe que adciona os inimigos que ATIRAM 
+	public void addenemy1() {
+
+		
+
+			enemies1.add(enemies1(RAND.nextInt(mundo.DIM_X - 40), -40)); // x = cima... y = lados
+			root.getChildren().add((Node) enemies1.get(b));
+			b += 1;
+			enemies1.add(enemies1(RAND.nextInt(mundo.DIM_X - 40), -40)); // x = cima... y = lados
+			root.getChildren().add((Node) enemies1.get(b));
+			b += 1;
+
+		
 
 	}
 
 	public void gameUpdate() {
 
-		// atualizaçao do tiro / animaçao
+		// atualizacao do tiro / animacao
 		aviaoShootUpdate();
+		enemiesMove(2);
+		enemiesDeath();
 
 	}
+	
 
-	public Circle shoot(double x, double y) {
+
+	public Circle shoot(double x, double y) {	
+			
+		
 		Circle c = new Circle();
-		c.setFill(Color.SPRINGGREEN);
+		c.setFill(Color.SPRINGGREEN);		
 		c.setLayoutX(x);
 		c.setLayoutY(y);
 		c.setRadius(4);
+		
 		return c;
 	}
 
@@ -157,7 +224,123 @@ public class program extends Application {
 		}
 	}
 
+	public ImageView enemies0(double x, double y) {
+		ImageView i = new ImageView(new Image(getClass().getResourceAsStream("recursos/enemy0.png")));
+		i.setLayoutX(x);
+		i.setLayoutY(y);
+		i.setFitHeight(50);
+		i.setFitWidth(50);
+		return i;
+	}
+
+	public ImageView enemies1(double x, double y) {
+		ImageView j = new ImageView(new Image(getClass().getResourceAsStream("recursos/enemy1.png")));
+		j.setLayoutX(x);
+		j.setLayoutY(y);
+		j.setFitHeight(50);
+		j.setFitWidth(50);
+		return j;
+	}
+
+	public void enemiesMove(double speed) {
+		// movendo inimigos
+		
+
+		if (dotR.getLayoutX() >= 40) { // limite >>> // inimigos normais
+
+			for (int i = 0; i < enemies0.size(); i++) {
+				enemies0.get(i).setLayoutY(enemies0.get(i).getLayoutY() + speed);
+
+			}
+		}
+		if (dotR.getLayoutX() >= 40) { // limite >>> // inimigos que atiram
+
+			for (int i = 0; i < enemies1.size(); i++) {
+				enemies1.get(i).setLayoutY(enemies1.get(i).getLayoutY() + speed);
+
+			}
+		}
+
+		
+		dotR.setLayoutX(dotR.getLayoutX() + speed);
+	}
+
+	private void addInimigo0() {
+
+
+		timer2.scheduleAtFixedRate(new TimerTask() {
+
+			@Override
+			public void run() {
+				Platform.runLater(() -> {
+
+					addenemy0();
+
+				});
+			}
+		}, 2000, 2000);
+
+		// DELEY > tempo de espera antes da 1 execucao da tarefa.
+		// INTERVAL > intervalo no qual a tarefa sera executada.
+	}
+	
+	private void addInimigo1() {
+
+		
+
+		timer2.scheduleAtFixedRate(new TimerTask() {
+
+			@Override
+			public void run() {
+				Platform.runLater(() -> {
+
+					addenemy1();
+
+				});
+			}
+		}, 5000, 9000);
+		// DELEY > tempo de espera antes da 1 execucao da tarefa.
+		// INTERVAL > intervalo no qual a tarefa sera executada.
+	}
+
+	private void enemiesDeath() {
+		// verefica se inimigo foi morto e retira ele >> inimigo normal
+		for (int i = 0; i < pShoots.size(); i++) {
+			for (int j = 0; j < enemies0.size(); j++) {
+				if (((pShoots.get(i).getLayoutX() > enemies0.get(j).getLayoutX())
+						&& ((pShoots.get(i).getLayoutX() < enemies0.get(j).getLayoutX() + 50))
+						&& ((pShoots.get(i).getLayoutY() > enemies0.get(j).getLayoutY())
+								&& ((pShoots.get(i).getLayoutY() < enemies0.get(j).getLayoutY() + 50))))) {
+
+					pontos += 100;
+					root.getChildren().remove(enemies0.get(j));
+					enemies0.remove(j);
+					root.getChildren().remove(pShoots.get(i));
+					pShoots.remove(i);
+
+				}
+			}
+		}
+		// verefica se inimigo foi morto e retira ele >> inimigo que atira
+		for (int i = 0; i < pShoots.size(); i++) {
+			for (int j = 0; j < enemies1.size(); j++) {
+				if (((pShoots.get(i).getLayoutX() > enemies1.get(j).getLayoutX())
+						&& ((pShoots.get(i).getLayoutX() < enemies1.get(j).getLayoutX() + 50))
+						&& ((pShoots.get(i).getLayoutY() > enemies1.get(j).getLayoutY())
+								&& ((pShoots.get(i).getLayoutY() < enemies1.get(j).getLayoutY() + 50))))) {
+
+					pontos += 200;
+					root.getChildren().remove(enemies1.get(j));
+					enemies1.remove(j);
+					root.getChildren().remove(pShoots.get(i));
+					pShoots.remove(i);
+				}
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		launch(args);
+
 	}
 }
